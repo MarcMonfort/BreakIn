125156a371e7b6e0e011b84175fd3e2ce3f63914
Jordi Cluet <jordi.cluet.martinell@gmail.com>
2020-10-15 01:26:02 +0200

    Jugador en forma d'ull
    El jugador és ara un parell d'ulls. De moment la imatge és sempre la mateixa, cal elaborar imatges amb els ulls mirant a altres costats si es vol fer que vagi canviant. Per això he fet que vagi en la direcció que vagi, sempre mostri la mateixa part de l'sprite.

--- Break_In_MSX/2DGame/Break_In/Break_In/Player.cpp
+++ Break_In_MSX/2DGame/Break_In/Break_In/Player.cpp
@@ -19,7 +19,7 @@ enum PlayerAnims
 
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
-	spritesheet.loadFromFile("images/bub.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	spritesheet.loadFromFile("images/ulls_ampliats.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25, 0.25), &spritesheet, &shaderProgram);
 	sprite->setNumberAnimations(4);
 	
@@ -27,17 +27,20 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 		sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
 		
 		sprite->setAnimationSpeed(STAND_RIGHT, 8);
-		sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.f));
+		//sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.f));
+		sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.f, 0.f));
 		
 		sprite->setAnimationSpeed(MOVE_LEFT, 8);
+		//sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
+		//sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
+		//sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
 		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
 		
 		sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
+		//sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
+		//sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
+		//sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.f, 0.f));
 		
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
