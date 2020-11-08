9d46b5e3518f219a7ac516cc2e77f1ae89f31155
Jordi Cluet <45299503+Makanut009@users.noreply.github.com>
2020-11-08 14:34:51 +0100

    Added credits class

---
+++ Break_In_MSX/2DGame/Break_In/Break_In/CreditsGameState.cpp
@@ -0,0 +1,107 @@
+#include "CreditsGameState.h"
+#include <iostream>
+#include <glm\gtc\matrix_transform.hpp>
+#include "Game.h"
+
+
+void CreditsGameState::init()
+{
+	initShaders();
+
+	spritesheet.loadFromFile("images/Credits3.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	background = Sprite::createSprite(glm::ivec2(SCREEN_WIDTH, SCREEN_HEIGHT), glm::vec2(1.f, 1.f), &spritesheet, &texProgram);
+	background->setPosition(glm::vec2(0, 0));
+
+	projection = glm::ortho(0.f, float(SCREEN_WIDTH - 1), float(SCREEN_HEIGHT - 1), 0.f);
+}
+
+void CreditsGameState::update(int deltaTime)
+{
+
+}
+
+void CreditsGameState::render()
+{
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glm::mat4 modelview;
+	texProgram.use();
+	texProgram.setUniformMatrix4f("projection", projection);
+	texProgram.setUniform4f("color", 1.0f, 1.0f, 1.0f, 1.0f);
+	modelview = glm::mat4(1.0f);
+	texProgram.setUniformMatrix4f("modelview", modelview);
+	texProgram.setUniform2f("texCoordDispl", 0.f, 0.f);
+	background->render();
+
+}
+
+
+void CreditsGameState::keyPressed(int key)
+{
+	if (key == 27) // Escape code
+	{
+		//MenuGameState::instance().init();
+		Game::instance().popGameState(); //or better push so we dont loose the state??
+		Game::instance().pushGameState(&MenuGameState::instance());
+	}
+
+	keys[key] = true;
+}
+
+void CreditsGameState::keyReleased(int key)
+{
+	keys[key] = false;
+}
+
+void CreditsGameState::specialKeyPressed(int key)
+{
+	specialKeys[key] = true;
+}
+
+void CreditsGameState::specialKeyReleased(int key)
+{
+	specialKeys[key] = false;
+}
+
+void CreditsGameState::mouseMove(int x, int y)
+{
+}
+
+void CreditsGameState::mousePress(int button)
+{
+}
+
+void CreditsGameState::mouseRelease(int button)
+{
+}
+
+
+
+void CreditsGameState::initShaders()
+{
+	Shader vShader, fShader;
+
+	vShader.initFromFile(VERTEX_SHADER, "shaders/texture.vert");
+	if (!vShader.isCompiled())
+	{
+		cout << "Vertex Shader Error" << endl;
+		cout << "" << vShader.log() << endl << endl;
+	}
+	fShader.initFromFile(FRAGMENT_SHADER, "shaders/texture.frag");
+	if (!fShader.isCompiled())
+	{
+		cout << "Fragment Shader Error" << endl;
+		cout << "" << fShader.log() << endl << endl;
+	}
+	texProgram.init();
+	texProgram.addShader(vShader);
+	texProgram.addShader(fShader);
+	texProgram.link();
+	if (!texProgram.isLinked())
+	{
+		cout << "Shader Linking Error" << endl;
+		cout << "" << texProgram.log() << endl << endl;
+	}
+	texProgram.bindFragmentOutput("outColor");
+	vShader.free();
+	fShader.free();
+}
---
+++ Break_In_MSX/2DGame/Break_In/Break_In/CreditsGameState.h
@@ -0,0 +1,50 @@
+#ifndef _CREDITSGAMESTATE_INCLUDE
+#define _CREDITSGAMESTATE_INCLUDE
+
+#include "GameState.h"
+#include "ShaderProgram.h"
+#include "Sprite.h"
+#include "SoundManager.h"
+
+
+
+class CreditsGameState : public GameState
+{
+
+public:
+	static CreditsGameState& instance()
+	{
+		static CreditsGameState M;
+
+		return M;
+	}
+
+	void init();
+	void update(int deltaTime);
+	void render();
+
+	// Input callback methods
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void specialKeyPressed(int key);
+	void specialKeyReleased(int key);
+	void mouseMove(int x, int y);
+	void mousePress(int button);
+	void mouseRelease(int button);
+
+
+
+private:
+	void initShaders();
+
+	Sprite* background;
+	Texture spritesheet;
+
+
+	ShaderProgram texProgram;
+	float currentTime;
+	glm::mat4 projection;
+};
+
+
+#endif
